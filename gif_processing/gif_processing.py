f3f478410ce3ef403dd3a501cf9ba48b9e358346
blomaxel <bloaxel@chalmers.se>
2023-05-12 17:32:23 +0200

    A good start. This part should not require much more work

--- gif_processing/gif_processing.py
+++ gif_processing/gif_processing.py
@@ -62,6 +62,7 @@ class GifFlipper:
 		self.frame_durations = []
 		self.faces = []
 		self.coordinates = []
+		self.flipped_frames = []
 
 	def load_frames(self, path):
 		# Resets all variables
@@ -76,6 +77,9 @@ class GifFlipper:
 
 		# Extracts total number of frames
 		self.n_frames = gif.n_frames
+
+		# Extracts frame durations
+		self.frame_durations = [gif.info['duration'] for i in range(self.n_frames)]
 
 		for i in range(self.n_frames):
 			frame = ImageTk.PhotoImage(gif)
@@ -108,11 +112,33 @@ class GifFlipper:
 	
 	def detect_faces(self):
 		#This function assumes that the frames have been loaded
-		for frame in self.frames:
-			faces_cut_out, coordinates = self.list_of_faces(frame)
-			self.faces.append(faces_cut_out)
+		for i in range(len(self.frames)):
+			faces_cut_out, coordinates = self.list_of_faces(self.frames[i])
+			self.faces[i] = (faces_cut_out)
 			self.coordinates.append(coordinates)
 
+	def flip_faces(self):#This funcition is not tested yet
+		#This function assumes that the frames have been loaded
+		#and that the faces have been detected
+		for i in range(self.n_frames):
+			flipped_frame = self.frames[i].copy()
+			for j in range(len(self.faces[i])):
+				#####Here we flip the faces happy/sad##### 
+				#temporarily, we just flip the faces horisontally
+				flipped_face = np.fliplr(self.faces[i][j])
+				flipped_frame.paste(Image.fromarray(flipped_face), tuple(self.coordinates[i][j][0][0:2])) #width and height not necessary here
+			self.flipped_frames.append(flipped_frame)
+	
+	def build_flipped_gif(self, path):
+		#Build GIF from flipped_frames and save to path
+		#This function assumes that the frames have been loaded
+		#and that the faces have been detected and flipped
+		#and that the flipped frames have been built
+		#and that the frame durations have been loaded
+		flipped_gif = Image.new('RGB', self.flipped_frames[0].size)
+		flipped_gif.save(path, save_all=True, append_images=self.flipped_frames[:-1], duration=self.frame_durations, loop=0)
+		
+		
 
 if __name__ == "__main__":
 
@@ -120,14 +146,18 @@ if __name__ == "__main__":
 	script_path = os.path.dirname(os.path.realpath(__file__))
 	#Initiate the flipper
 	gif_flipper = GifFlipper()
+
 	gif_path = os.path.join(script_path, "data/mike.gif")
 	#View the original gif once
 	GifViewer(gif_path).view_gif()
 
 	gif_flipper.load_frames(gif_path)
 	gif_flipper.detect_faces()
-	print(f"the length of the faces list is {len(gif_flipper.faces)} and the last element looks like this: {gif_flipper.faces[-1]}")
-	print(f"the length of the coordinates list is {len(gif_flipper.coordinates)} and the last element looks like this: {gif_flipper.coordinates[-1][-1]}")
-	print(np.shape(gif_flipper.faces[-5][-1]))	
-	print(np.shape(gif_flipper.coordinates[-1][-1]))
+	gif_flipper.flip_faces()
+	flipped_gif_path = os.path.join(script_path, "output/mike_flipped.gif")
+	gif_flipper.build_flipped_gif(flipped_gif_path)
+	#View the flipped gif once
+	#GifViewer(flipped_gif_path).view_gif()
+
+
 
